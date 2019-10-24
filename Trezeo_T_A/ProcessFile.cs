064c05d1c7f07205e7a62ecebe42aad266a6b818
Jose Miralles <jemiralleslopez@gmail.com>
2019-10-24 15:43:21 +0100

    Add project files.

---
+++ Trezeo_T_A/ProcessFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Trezeo_T_A
+{
+    public class ProcessFile
+    {
+        public void Word_Unique_First(string fileName)
+        {
+            var wordDictionary = new Dictionary<string, int>();
+            Console.WriteLine(DateTime.Now);
+            using (StreamReader reader = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                string word = "";
+                while (!reader.EndOfStream)
+                {
+                    var character = (char)reader.Read();
+                    if (Char.IsLetterOrDigit(character))
+                    {
+                        word += character;
+                    }
+                    else
+                    {
+                        word = word.ToLower();
+                        if (wordDictionary.ContainsKey(word))
+                        {
+                            wordDictionary[word]++;
+                        }
+                        else
+                        {
+                            wordDictionary.Add(word, 1);
+                        }
+                        word = "";
+                    }
+                }
+            }
+            Console.WriteLine(wordDictionary.FirstOrDefault(x => x.Value == 1).Key);
+            Console.WriteLine(DateTime.Now);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Trezeo_T_A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Trezeo_T_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processFile = new ProcessFile();
+            processFile.Word_Unique_First("test.txt");
+        }
+    }
+}
